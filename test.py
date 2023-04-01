a0e8dedc10ecf312f4963c160667e87e984bd5d2
ISYhunny <128983174+ISYhunny@users.noreply.github.com>
2023-04-01 23:15:52 +0200

    nice code :)

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-#Ideas:
-#1) go the the pages and get
-#    date, game, refs
-#2) store all in csv file
-#3) get past data 
-#4) store past data as well
-#5) check data - empty lines
-#6) run statistics and plots :)
-
 #https://docs.python.org/3/tutorial/venv.html
 #In powershell als admin:
 #get-ExecutionPolicy
@@ -24,8 +15,6 @@
 #python -m pip install "XxX"
 #To see all packages: 
 #python -m pip list 
-
-
 
 #6) run statistics and plots :)
 
@@ -59,9 +48,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver import ActionChains
 
-url = "https://www.sfl.ch/spielplan/"
-csvFile = "spielplan.csv"
-
 def driver_setup(url):
     global driver
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -73,81 +59,101 @@ def driver_setup(url):
 def driver_quit():
     driver.quit()
 
+def open_or_create_csv_file(csvFile):
+    global datei
+    dir_list = os.listdir()
+    if (csvFile in dir_list):
+        try:
+            datei = open(csvFile,"w")
+        except:
+            print("Dateizugriff nicht erfolgreich")
+            sys.exit(0)
+    else:
+        with open(csvFile, 'w') as datei:
+            pass
+    return datei
+
+def write_in_csv_file(infos):
+    for i in infos:
+        datei.write(i + ',')
+    datei.write('\n')
+
+def close_csv_file():
+    datei.close
+
+def scroll_down_press_forward():
+    mouse = ActionChains(driver)
+    start_point = ScrollOrigin.from_viewport(0,0)
+    show_more = driver.find_element(By.PARTIAL_LINK_TEXT, "Mehr anzeigen")
+    for i in range(2):
+        for j in range(15):
+            ActionChains(driver).scroll_from_origin(start_point, 0, 500).perform
+            time.sleep(1)
+        try:
+            mouse.move_to_element(show_more).click().perform()
+            time.sleep(1)
+        except:
+            print('1')
 
 def get_page_source_and_create_soup():
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
     return soup
 
-
-def get_all_href_from_urls(all_url, soup):
-    all_urls = [a['href']
-        for a in soup('a')
-        if a.has_attr('href')]
+def get_all_href_from_urls(soup):
+    all_urls = [a['href'] for a in soup('a') if a.has_attr('href')]
     return all_urls
 
-def filter_relevant_href(all_urls):
-    includes_start = r"^https://www.sfl.ch/spieldetail/detail/"
-    relevant_urls = [url  
-        for url in all_urls
-        if re.match(includes_start, url)]
+def filter_relevant_href(all_urls, myFilter):
+    relevant_urls = [url for url in all_urls if re.match(myFilter, url)]
     relevant_urls = list(set(relevant_urls))
-
-def open_or_create_csv_file():
-    global d
-    dir_list = os.listdir()
-    if (csvFile in dir_list):
-        try:
-            d = open(csvFile,"w")
-        except:
-            print("Dateizugriff nicht erfolgreich")
-            sys.exit(0)
-    else:
-        with open(csvFile, 'w') as d:
-            pass
-
-def close_csv_file(d):
-    d.close
-
-
-#scroll down & press 'Mehr anzeigen'
-scroll_origin = ScrollOrigin.from_viewport(0,0)
-for i in range(1,3):
-    for i in range(1,15):
-        ActionChains(driver)\
-            .scroll_from_origin(scroll_origin, 0, 500)\
-            .perform()
-        time.sleep(1)
-
-    try:
-        mouse = ActionChains(driver)
-        show_more = driver.find_element(By.LINK_TEXT, 'Mehr anzeigen')
-        mouse.move_to_element(show_more).click().perform()
-        time.sleep(1)
-    except:
-        print("1")
+    return relevant_urls
 
 # get the actual referees from the page
-for i in range(len(relevant_urls)):
-    driver.get(relevant_urls[i])
-    page_source2 = driver.page_source
-    soup2 = BeautifulSoup(page_source2, 'html.parser')
+def find_all_infos(soup):
     try:
-        ref_tag = soup2.find('strong', string="SCHIEDSRICHTER")
-        #print(ref_tag)
+        ref_tag = soup.find('strong', string="SCHIEDSRICHTER")
         ref1 = ref_tag.next_sibling.next_sibling
         ref2 = ref1.next_sibling.next_sibling
         ref3 = ref2.next_sibling.next_sibling
         ref4 = ref3.next_sibling.next_sibling
-        datum = soup2.find('div', class_="c-matchdetail-hero__date u-text-semibold u-text-center").next.next.next
+        datum = soup.find('div', class_="c-matchdetail-hero__date u-text-semibold u-text-center").next.next.next
         datum = (''.join(datum.splitlines())).lstrip()
-        runde = soup2.find('div', class_="c-matchdetail-hero__round u-text-center u-text-semibold").next
-        home = soup2.find('p', class_="u-visible-md-up").next
+        runde = soup.find('div', class_="c-matchdetail-hero__round u-text-center u-text-semibold").next
+        home = soup.find('p', class_="u-visible-md-up").next
         away = home.find_next('p', class_="u-visible-md-up").next
-
-        d.write(runde + "," + datum + "," + home + "," + away + "," +  ref1 + "," + ref2 + "," + ref3 + "," + ref4 + "\n")
+        spielinfos = [runde, datum, home, away, ref1, ref2, ref3, ref4]
+        return spielinfos
     except:
         pass
+    
+def main():
+    url = "https://www.sfl.ch/spielplan/"
+    csvFile = "test.csv"
+    myFilter = r"^https://www.sfl.ch/spieldetail/detail/"
+    
+    open_or_create_csv_file(csvFile)
+    driver_setup(url)
+    scroll_down_press_forward()
+    soup = get_page_source_and_create_soup()
+    all_urls = get_all_href_from_urls(soup)
+    relevant_urls = filter_relevant_href(all_urls, myFilter)
+    for i in range(len(relevant_urls)):
+        driver.get(relevant_urls[i])
+        soup = get_page_source_and_create_soup()
+        infos = find_all_infos(soup)
+        try:
+            write_in_csv_file(infos)
+        except:
+            pass
+    close_csv_file()
+    driver_quit()
+
+main()
+
+
+
+
 
 
         
